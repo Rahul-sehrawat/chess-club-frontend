be38ee4f0037b102dd4c23a5cd134291973d6bc4
inv-amalusudhakaran <amalu.sudhakaran@innovaturelabs.com>
2024-09-07 14:31:55 +0530

    send the count of players online to the frontend.

--- src/screens/Landing.tsx
+++ src/screens/Landing.tsx
@@ -1,7 +1,41 @@
 import { useNavigate } from "react-router-dom"
 import { Button } from "../components/PlayButton";
+import { useSocket } from "../hooks/useSocket";
+import { useEffect, useState } from "react";
 
 export const Landing = () => {
+    const socket = useSocket();
+    const [playersActive, setPlayersActive] = useState<number | null>(null); // State for storing number of active players
+   
+    useEffect(() => {
+        if (!socket) {
+            console.log("Socket not connected");
+            return;
+        }
+        socket.send(JSON.stringify({ type: 'READY', message: "WebSocket is ready to receive messages" }));
+        socket.onmessage = (event) => {
+            if (!event || !event.data) {
+                return;
+            }
+            try {
+                const message = JSON.parse(event.data);
+                if (message.type === 'PLAYERS_ACTIVE') {
+                    console.log('Number of players active Now', message.message);
+                    setPlayersActive(message.message);
+
+                } else {
+                    console.log('message from server:', message);
+                }
+            } catch (err) {
+                console.error("Error parsing message from WebSocket:", err);
+            }
+        };
+        return () => {
+            if (socket) {
+                socket.close();
+            }
+        };
+    }, [socket]);
     const navigate = useNavigate();
     return <div className=" w-full h-full flex justify-center items-start pt-12 " >
             <div className=" border-black  bg-black bg-opacity-80 border-4 h-3/4  w-3/4  rounded-xl flex  justify-evenly p-10 shadow-[5px_5px_0px_0px_rgba(255,255,255)]   " >
@@ -14,7 +48,7 @@ export const Landing = () => {
                         <h1 className=" text-4xl font-bold text-white">Play Chess Online  </h1>
                         <div className="flex justify-center gap-10 ">
                         <h2 className="text-white pt-8 font-bold">-- Games Today</h2>
-                        <h2 className="text-white pt-8 font-bold"> -- Playing Now</h2>
+                        <h2 className="text-white pt-8 font-bold">{playersActive !== null ? playersActive : '--'} Playing Now</h2>
                         </div>
                        <div className=" flex flex-col gap-10  m-12    ">
                         <Button  onClick={() => {
